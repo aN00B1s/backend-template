11b9f301ce26f797868a0c970ebd4372b69f2c23
Dmitry Kurbatov <anubchik@gmail.com>
2023-11-07 04:48:45 +0300

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import 'source-map-support/register'
+import 'source-map-support/register.js'
 import * as queries from '#src/app.queries'
 import logger from '#src/utils/log'
 import { db } from '#src/utils/core'
